c2c2966a3341dc5880cf01e20f71cca22af4ea8c
mjrb <www.w.com123@gmail.com>
2019-04-14 20:11:16 -0400

    implement get line

--- keyboard.cpp
+++ keyboard.cpp
@@ -8,7 +8,7 @@ KeyboardDriver::KeyboardDriver(InterruptManager* manager) :
   }
   // tell keybaord to use interrupts
   com.write(0xAE);
-  
+
   // change state to ??
   // get current state
   com.write(0x20);
@@ -22,13 +22,100 @@ KeyboardDriver::KeyboardDriver(InterruptManager* manager) :
 }
 
 KeyboardDriver::~KeyboardDriver() {
-  
+
 }
+
+uint8_t KeyboardDriver::buffer[GETC_BUF_SIZE];
+uint32_t KeyboardDriver::head = 0;
+uint32_t KeyboardDriver::count = 0;
+
+#define KMAP(code, c) case code: key = c; break;
 
 uint32_t KeyboardDriver::operator()(uint32_t esp) {
   uint8_t key = data.read();
-  printf("keypress ");
-  printh(key);
-  printf("\n");
+  switch(key) {
+    KMAP(0x2, '1');
+    KMAP(0x3, '2');
+    KMAP(0x4, '3');
+    KMAP(0x5, '4');
+    KMAP(0x6, '5');
+    KMAP(0x7, '6');
+    KMAP(0x8, '7');
+    KMAP(0x9, '8');
+    KMAP(0xA, '9');
+    KMAP(0xB, '0');
+    KMAP(0xE, '\b');
+
+    KMAP(0x10, 'q');
+    KMAP(0x11, 'w');
+    KMAP(0x12, 'e');
+    KMAP(0x13, 'r');
+    KMAP(0x14, 't');
+    KMAP(0x15, 'y');
+    KMAP(0x16, 'u');
+    KMAP(0x17, 'i');
+    KMAP(0x18, 'o');
+    KMAP(0x19, 'p');
+
+    KMAP(0x1E, 'a');
+    KMAP(0x1F, 's');
+    KMAP(0x20, 'd');
+    KMAP(0x21, 'f');
+    KMAP(0x22, 'g');
+    KMAP(0x23, 'h');
+    KMAP(0x24, 'j');
+    KMAP(0x25, 'k');
+    KMAP(0x26, 'l');
+    KMAP(0x1C, '\n');
+
+    KMAP(0x2C, 'z');
+    KMAP(0x2D, 'x');
+    KMAP(0x2E, 'c');
+    KMAP(0x2F, 'v');
+    KMAP(0x30, 'b');
+    KMAP(0x31, 'n');
+    KMAP(0x32, 'm');
+
+    KMAP(0x39, ' ');
+  default:
+    if (key > 0x80) {
+      // key up code
+      return esp;
+    }
+    key = '?';
+  }
+  if (count < GETC_BUF_SIZE) {
+    uint32_t index = (head + count) % GETC_BUF_SIZE;
+    buffer[index] = key;
+    count++;
+  }
   return esp;
+}
+
+extern "C" uint8_t getc() {
+  while (KeyboardDriver::count == 0) {
+    asm volatile("hlt");
+  }
+  uint8_t result = KeyboardDriver::buffer[KeyboardDriver::head];
+  KeyboardDriver::head++;
+  KeyboardDriver::count--;
+  return result;
+}
+
+extern "C" uint8_t get_line(char* buf, uint32_t length) {
+  uint8_t c = getc();
+  int i = 0;
+  while (c != '\n' && i < length - 1) {
+    buf[i] = c;
+    putc(c);
+    c = getc();
+    i++;
+  }
+  if (c == '\n') {
+    putc('\n');
+    if (i < length - 1) {
+      buf[i] = '\n';
+    }
+  }
+  buf[length - 1] = '\0';
 }
--- keyboard.h
+++ keyboard.h
@@ -6,13 +6,27 @@
 #include "port.h"
 #include "print.h"
 
+#define GETC_BUF_SIZE 256
+
 class KeyboardDriver : public InterruptHandler {
   Port8slow data;
   Port8slow com;
 public:
+  // fixed circular buffer/queue to store read chars
+  // if we go past the max size keypresses are just droped
+  static uint8_t buffer[GETC_BUF_SIZE];
+  static uint32_t head;
+  static uint32_t count;
+
   KeyboardDriver(InterruptManager* manager);
   ~KeyboardDriver();
   virtual uint32_t operator()(uint32_t esp);
 };
+
+extern "C" uint8_t getc();
+
+// length includes \0
+// garuntees buf ends with \0
+extern "C" uint8_t get_line(char* buf, uint32_t length);
 
 #endif

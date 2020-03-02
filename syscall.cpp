ed5a9d9b4c9adf3eba62c1ce24fb42f6816ce639
mjrb <www.w.com123@gmail.com>
2020-03-02 11:03:33 -0500

    redo syscalls to unify console and keyboard with file interface

--- syscall.cpp
+++ syscall.cpp
@@ -4,6 +4,8 @@ uint32_t SYS_EAX;
 uint32_t SYS_EBX;
 uint32_t SYS_ECX;
 uint32_t SYS_EDX;
+uint32_t SYS_ESI;
+uint32_t SYS_EDI;
 
 SyscallHandler::SyscallHandler(InterruptManager* manager, JumpFS* jfs) :
   InterruptHandler(0xCD, manager), jfs(jfs) {};
@@ -11,34 +13,40 @@ SyscallHandler::~SyscallHandler() {};
 
 uint32_t SyscallHandler::operator()(uint32_t esp) {
   static File* open_files[255];
-  static uint16_t next = 0;
+  static uint16_t next = 2;
+  static ConsoleStdout stdout;
+  static KeyboardStdin stdin;
+
+  open_files[0] = &stdin;
+  open_files[1] = &stdout;
 
   switch (SYS_EAX) {
   case 0:
     // exit
     break;
   case 1:
-    // printf
-    printf((char*) SYS_EBX);
+    // read
+    open_files[SYS_EBX]->read(SYS_ECX, (uint8_t*)SYS_EDI, SYS_EDX);
     break;
   case 2:
-    putc((char)SYS_EBX);
+    // write
+    open_files[SYS_EBX]->write(SYS_ECX, (uint8_t*)SYS_ESI, SYS_EDX);
     break;
   case 3:
-    // getc
-    SYS_EAX = getc();
-    break;
-  case 4:
-    open_files[next] = jfs->open((char*)SYS_EBX);
+    // open
+    open_files[next] = jfs->open((char*)SYS_ESI);
     if (open_files[next] == 0) {
       SYS_EAX = 0xFF; // -1
       break;
     }
     SYS_EAX = next;
     next++;
-  case 5:
-    open_files[SYS_EAX]->exec((uint8_t*)SYS_ECX, SYS_EBX);
+  case 4:
+    // exec
+    open_files[SYS_EBX]->exec((uint8_t*)SYS_ESI, SYS_ECX);
     break;
+  case 5:
+    // close
   default:
     printf("REG ");
     printh(SYS_EAX);
@@ -46,6 +54,12 @@ uint32_t SyscallHandler::operator()(uint32_t esp) {
     printh(SYS_EBX);
     printf(" ");
     printh(SYS_ECX);
+    printf("\n");
+    printh(SYS_EDX);
+    printf(" ");
+    printh(SYS_ESI);
+    printf(" ");
+    printh(SYS_EDI);
     printf("\n");
   }
   return esp;
